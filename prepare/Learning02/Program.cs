49c25fbc5352ec8586824a24837a209fe70d4112
Landon Delaney <delaneyboys16@gmail.com>
2023-06-07 13:58:37 -0600

    addig homework

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Collections.Generic;
+
+public class Job
+{
+    public string Company { get; private set; }
+    public string Title { get; private set; }
+    public int StartYear { get; private set; }
+    public int EndYear { get; private set; }
+
+    public Job(string company, string title, int startYear, int endYear)
+    {
+        Company = company;
+        Title = title;
+        StartYear = startYear;
+        EndYear = endYear;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} ({Company}) {StartYear}-{EndYear}";
+    }
+}
+
+public class Resume
+{
+    public string Name { get; private set; }
+    public List<Job> Jobs { get; private set; }
+
+    public Resume(string name)
+    {
+        Name = name;
+        Jobs = new List<Job>();
+    }
+
+    public void AddJob(Job job)
+    {
+        Jobs.Add(job);
+    }
+
+    public void DisplayResume()
+    {
+        Console.WriteLine("Resume for: " + Name);
+        foreach (var job in Jobs)
+        {
+            Console.WriteLine(job.ToString());
+        }
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume resume = new Resume("John Doe");
+        resume.AddJob(new Job("Microsoft", "Software Engineer", 2019, 2022));
+        resume.AddJob(new Job("Google", "Senior Software Engineer", 2022, 2023));
+        
+        resume.DisplayResume();
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,106 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-class Program
+public class Entry
 {
-    static void Main(string[] args)
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public class Journal
+{
+    public List<Entry> Entries { get; set; } = new List<Entry>();
+    private List<string> Prompts { get; set; } = new List<string>
     {
-        Console.WriteLine("Hello Develop02 World!");
+        "Who was the most interesting person I interacted with today?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?"
+    };
+
+    public void WriteEntry()
+    {
+        var random = new Random();
+        var prompt = Prompts[random.Next(Prompts.Count)];
+        Console.WriteLine(prompt);
+        var response = Console.ReadLine();
+        Entries.Add(new Entry { Prompt = prompt, Response = response, Date = DateTime.Now });
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (var entry in Entries)
+        {
+            Console.WriteLine($"Date: {entry.Date}, Prompt: {entry.Prompt}, Response: {entry.Response}");
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (var writer = new StreamWriter(filename))
+        {
+            foreach (var entry in Entries)
+            {
+                writer.WriteLine($"Date: {entry.Date}, Prompt: {entry.Prompt}, Response: {entry.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        Entries.Clear();
+        using (var reader = new StreamReader(filename))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                var parts = line.Split(new[] { ", Prompt: ", ", Response: " }, StringSplitOptions.None);
+                var date = DateTime.Parse(parts[0].Replace("Date: ", ""));
+                var prompt = parts[1];
+                var response = parts[2];
+                Entries.Add(new Entry { Date = date, Prompt = prompt, Response = response });
+            }
+        }
+    }
+}
+
+public class Program
+{
+    static void Main()
+    {
+        var journal = new Journal();
+        while (true)
+        {
+            Console.WriteLine("1. Write a new entry\n2. Display the journal\n3. Save the journal to a file\n4. Load the journal from a file\n5. Exit");
+            var option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    journal.WriteEntry();
+                    break;
+                case "2":
+                    journal.DisplayEntries();
+                    break;
+                case "3":
+                    Console.WriteLine("Enter a filename:");
+                    var saveFilename = Console.ReadLine();
+                    journal.SaveToFile(saveFilename);
+                    break;
+                case "4":
+                    Console.WriteLine("Enter a filename:");
+                    var loadFilename = Console.ReadLine();
+                    journal.LoadFromFile(loadFilename);
+                    break;
+                case "5":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
     }
 }
